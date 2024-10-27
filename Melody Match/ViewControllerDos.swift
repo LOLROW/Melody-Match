4998fa6d2014d5905a5a18b70344e9fbc83cacb4
LOLROW <owen.strader2@gmail.com>
2024-10-27 00:49:14 -0500

    App design and setup
    Added extra labels, button, and created empty functions

---
+++ Melody Match/ViewControllerDos.swift
@@ -0,0 +1,40 @@
+//
+//  ViewControllerDos.swift
+//  Melody Match
+//
+//  Created by Owen Strader on 10/27/24.
+//
+
+import UIKit
+
+class ViewControllerDos: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    func displaySongTitle() {
+        
+    }
+    func displayGenre() {
+        
+    }
+    func displayConfidence() {
+        // Display confidence number
+    }
+    func getSongID() {
+        
+    }
+    @IBAction func playSongButton(_ sender: Any) {
+        
+    }
+    @IBAction func stopSong(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var songTitle: UILabel!
+    
+    @IBOutlet weak var genreLabel: UILabel!
+    
+    @IBOutlet weak var confidenceLevelLabel: UILabel!
+}
